3913e349e10f6cb3515cedc18b5989c95fa830a6
Claudyo <pasqaldyo@gmail.com>
2024-07-15 19:55:46 +0700

    penambahan bg

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <nav className="flex fixed justify-between w-full bg-[#222222] h-[6 vh] ">
+        <nav className="flex fixed justify-between w-full bg-[#222222] h-[6vh] ">
             <h1 className="text-white">Logo</h1>
             <div>
                 <ul className="flex">
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-screen w-full bg-black">
 
     </div>
   );
