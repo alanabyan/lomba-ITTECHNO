e2255a97ae829d04e89493806aaee1caf79cf586
Claudyo <pasqaldyo@gmail.com>
2024-07-15 19:37:06 +0700

    penambahan navbar

---
+++ app/navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav className="flex fixed justify-between w-full bg-[#222222] h-[6 vh] ">
+            <h1 className="text-white">Logo</h1>
+            <div>
+                <ul className="flex">
+                    <li className="text-white">Home</li>
+                    <li className="text-white">About</li>
+                    <li className="text-white">Impact</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
