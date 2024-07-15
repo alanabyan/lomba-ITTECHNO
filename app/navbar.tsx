9b223d2c3c7be0de10939b9327a223e04a26b3ce
Claudyo <pasqaldyo@gmail.com>
2024-07-15 20:04:59 +0700

    navbar code

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return (
-        <nav className="flex fixed justify-between w-full bg-[#222222] h-[6vh] ">
-            <h1 className="text-white">Logo</h1>
+        <nav className="flex fixed justify-between w-full bg-[#222222] h-[6vh] items-center ">
+            <h1 className="text-white px-6">Logo</h1>
             <div>
                 <ul className="flex">
-                    <li className="text-white">Home</li>
-                    <li className="text-white">About</li>
-                    <li className="text-white">Impact</li>
+                    <Link href={''}><li className="text-white px-5 cursor-pointer">Home</li></Link>
+                    <Link href={''}><li className="text-white px-5 cursor-pointer">About</li></Link>
+                    <Link href={''}><li className="text-white px-5 cursor-pointer">Impact</li></Link>
                 </ul>
             </div>
         </nav>
