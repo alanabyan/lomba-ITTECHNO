02afbddf4a47da7b91474a50ae5e8fb6674008c1
Claudyo <pasqaldyo@gmail.com>
2024-07-17 16:05:16 +0700

    yahaha

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav className="flex fixed justify-between w-full bg-[#222222] h-[6vh] items-center ">
+        <nav className="flex fixed justify-between w-full bg-[#0C3759] bg-opacity-50 h-[6vh] backdrop-blur items-center ">
             <h1 className="text-white px-6">Logo</h1>
             <div>
                 <ul className="flex">
-                    <Link href={''}><li className="text-white px-5 cursor-pointer">Home</li></Link>
-                    <Link href={''}><li className="text-white px-5 cursor-pointer">About</li></Link>
-                    <Link href={''}><li className="text-white px-5 cursor-pointer">Impact</li></Link>
+                    <Link href={''}><li className="text-white px-5 cursor-pointer font-semibold hover:text-[#4796d3] transition duration-150 ease-in">Home</li></Link>
+                    <Link href={''}><li className="text-white px-5 cursor-pointer font-bold hover:text-[#15609B] transition duration-150 ease-in">About</li></Link>
+                    <Link href={''}><li className="text-white px-5 cursor-pointer font-bold hover:text-[#15609B] transition duration-150 ease-in">Impact</li></Link>
                 </ul>
             </div>
         </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-black">
-
+    <div className="h-[200vh] w-full bg-[#092943]">
+      <div id="Home" className="text-white py-52 px-24 text-[3.2vh] ">AI Implementation in Software Development</div>
     </div>
   );
 }
