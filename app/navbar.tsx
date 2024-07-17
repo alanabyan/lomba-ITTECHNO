55ae2db3bb409c062a504c6e1903096a87d6883f
Claudyo <pasqaldyo@gmail.com>
2024-07-17 16:29:30 +0700

    perubahan navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -2,15 +2,17 @@ import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <nav className="flex fixed justify-between w-full bg-[#0C3759] bg-opacity-50 h-[6vh] backdrop-blur items-center ">
-            <h1 className="text-white px-6">Logo</h1>
-            <div>
-                <ul className="flex">
-                    <Link href={''}><li className="text-white px-5 cursor-pointer font-semibold hover:text-[#4796d3] transition duration-150 ease-in">Home</li></Link>
-                    <Link href={''}><li className="text-white px-5 cursor-pointer font-bold hover:text-[#15609B] transition duration-150 ease-in">About</li></Link>
-                    <Link href={''}><li className="text-white px-5 cursor-pointer font-bold hover:text-[#15609B] transition duration-150 ease-in">Impact</li></Link>
-                </ul>
-            </div>
-        </nav>
+        <main className="flex justify-center">
+            <nav className="flex fixed justify-between w-[40%] backdrop-filter-[40px] mt-10 h-[6vh] backdrop-blur items-center rounded-full bg-slate-600 bg-opacity-35">
+                <h1 className="text-white px-5  ">Logo</h1>
+                <div>
+                    <ul className="flex">
+                        <Link href={''}><li className="text-white px-5 cursor-pointer font-semibold hover:text-[#4796d3] transition duration-150 ease-in">Home</li></Link>
+                        <Link href={''}><li className="text-white px-5 cursor-pointer font-bold hover:text-[#15609B] transition duration-150 ease-in">About</li></Link>
+                        <Link href={''}><li className="text-white px-5 cursor-pointer font-bold hover:text-[#15609B] transition duration-150 ease-in">Impact</li></Link>
+                    </ul>
+                </div>
+            </nav>
+        </main>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-[200vh] w-full bg-[#092943]">
-      <div id="Home" className="text-white py-52 px-24 text-[3.2vh] ">AI Implementation in Software Development</div>
+    <div className="h-[200vh] w-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#093db5] via-[#0d1447] to-[#041640]">
+      <div id="Home">
+
+      </div>
+      <div  className="text-white py-52 px-24 text-[3.2vh] ">AI Implementation in Software Development</div>
     </div>
   );
 }
